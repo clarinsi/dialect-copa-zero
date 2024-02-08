527b46d5888485c281e237c6e30b492be9113dee
Nikola Ljubesic <nikola.ljubesic@ijs.si>
2024-02-08 16:32:04 +0100

    First concrete results

--- gpt/README.md
+++ gpt/README.md
@@ -1,3 +1,18 @@
+
+## gpt-3.5-turbo-0125
+
+| copa-hr.train | copa-mk.train | copa-mk.train.trans | copa-sl-cer.train | copa-sl.train | copa-sr-tor.train | copa-sr-tor.train.trans | copa-sr.train | copa-sr.train.trans |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| 0.843 | 0.733 | 0.713 | 0.535 | 0.79 | 0.715 | 0.772 | 0.795 | 0.825 |
+
+## gpt-4-0125-preview
+
+| copa-hr.train | copa-mk.train | copa-mk.train.trans | copa-sl-cer.train | copa-sl.train | copa-sr-tor.train | copa-sr-tor.train.trans | copa-sr.train | copa-sr.train.trans |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| 0.963 | 0.945 | 0.9 | 0.608 | 0.96 | 0.92 | 0.912 | 0.955 | 0.96 |
+
+```
 python query.py -i ../dialect-copa/copa-sr-tor/train.jsonl -o copa-sr-tor.train.jsonl -m gpt-3.5-turbo-0125
-python query.py -i ../dialect-copa/copa-sr-tor/train.trans.jsonl -o copa-sr-tor.train.trans.jsonl -m gpt-3.5-turbo-0125
-python eval.py -a gpt-3.5-turbo-0125/copa-sr-tor.train.jsonl.args
+python eval.py gpt-3.5-turbo-0125/copa-sr-tor.train.jsonl.args # for eval of a single dataset
+python eval.py gpt-3.5-turbo-0125 -a # for all datasets eval
+```
--- gpt/eval.py
+++ gpt/eval.py
@@ -2,37 +2,62 @@ import json
 import sys
 import argparse
 import os
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--argfile')
-#parser.add_argument('-o', '--output')
-#parser.add_argument('-m', '--model')
-#parser.add_argument('-s', '--sleep', default=2)
-args = parser.parse_args()
-argdict=json.load(open(args.argfile))
-responses=json.load(open(os.path.join(argdict['model'],argdict['output'])))
 from collections import Counter
-truth=[]
-clear=[]
-for line in open(argdict['input']):
-    entry=json.loads(line)
-    #print(entry['idx'])
-    #print(responses[entry['idx']])
-    #if len(responses)==entry['idx']:
-    #    break
-    try:
-        label=int(responses[entry['idx']])-1
-    except:
-        truth.append(False)
-        clear.append(False)
-        continue
-    truth.append(entry['label']==label)
-    clear.append(True)
-truth=Counter(truth)
-clear=Counter(clear)
-print(truth)
-print(clear)
-print(sum(clear.values()))
-print('accuracy',truth[True]/sum(truth.values()))
-print('lack of response',clear[False]/sum(clear.values()))
-print(str(truth[True]/sum(truth.values()))+'\t'+str(truth[True]/clear[True])+'\t'+str(clear[False]/sum(clear.values())))
+
+def count(argfile_path):
+    argdict=json.load(open(argfile_path))
+    responses_path=os.path.join(argdict['model'],argdict['output'])
+    train_path=argdict['input']
+    responses=json.load(open(responses_path))
+    truth=[]
+    clear=[]
+    for line in open(train_path):
+        entry=json.loads(line)
+        #print(entry['idx'])
+        #print(responses[entry['idx']])
+        if len(responses)==entry['idx']:
+            print('INCOMPLETE OUTPUT')
+            break
+        try:
+            label=int(responses[entry['idx']])-1
+        except:
+            truth.append(False)
+            clear.append(False)
+            continue
+        truth.append(entry['label']==label)
+        clear.append(True)
+    truth=Counter(truth)
+    clear=Counter(clear)
+    return truth,clear,truth[True]/sum(truth.values()),clear[False]/sum(clear.values())
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('argfile')
+    parser.add_argument('-t', '--table', action='store_true')
+    parser.add_argument('-a', '--all', action='store_true')
+    #parser.add_argument('-m', '--model')
+    #parser.add_argument('-s', '--sleep', default=2)
+    args = parser.parse_args()
+    if args.all:
+        model_path=args.argfile
+        header=[]
+        row=[]
+        for file in sorted(os.listdir(model_path)):
+            if file.endswith('.args'):
+                header.append(file[:-11])
+                _,_,a,l=count(os.path.join(model_path,file))
+                row.append(a)
+        print('| '+' | '.join(header)+' |')
+        print('| '+' | '.join(['---' for e in header])+' |')
+        print('| '+' | '.join([str(round(e,3)) for e in row])+' |')
+    else:
+        truth,clear,acc,lack=count(args.argfile)    
+        if not args.table:
+            print(truth)
+            print(clear)
+            print(sum(clear.values()))
+            print('accuracy',acc)
+            print('lack of response',lack)
+            print(str(truth[True]/sum(truth.values()))+'\t'+str(truth[True]/clear[True])+'\t'+str(clear[False]/sum(clear.values())))
+        else:
+            print("%.3f | %.3f" % (truth[True]/sum(truth.values()),clear[False]/sum(clear.values())))
